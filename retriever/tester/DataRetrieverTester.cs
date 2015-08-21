51c887f52232ecc202926743969bc0f6bce17505
khabilepravin <khabilepravin@gmail.com>
2015-08-21 10:09:58 -0400

    Start of the project
    Initial commit

---
+++ retriever/tester/DataRetrieverTester.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tester
+{
+    [TestClass]
+    public class DataRetrieverTester
+    {
+        [TestMethod]
+        public void ExecuteFetchTest()
+        {
+        }
+    }
+}
