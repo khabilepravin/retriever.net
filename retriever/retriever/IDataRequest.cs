a3bc61e034b4f56ee9504af6d6f6b7b196654f72
khabilepravin <khabilepravin@gmail.com>
2015-08-28 09:21:58 -0400

    Interface for datarequest type classes

---
+++ retriever/retriever/IDataRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Retriever.Net
+{
+    interface IDataRequest
+    {
+        string Fetch(string procName);
+        string Fetch(string procName, string jsonFetchParams);
+        string Update(string procName, string jsonData);
+        string Update(List<dynamic> objects);
+    }
+}
