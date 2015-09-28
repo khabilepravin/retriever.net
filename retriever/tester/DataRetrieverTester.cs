253077da6683139f341c06da6520a33ebc3a8482
khabilepravin <khabilepravin@gmail.com>
2015-09-28 09:01:53 -0400

    Updated initial tests for update and fetch

--- retriever/tester/DataRetrieverTester.cs
+++ retriever/tester/DataRetrieverTester.cs
@@ -13,7 +13,39 @@ namespace tester
         {
             SqlDataRequest dataRequest = new SqlDataRequest("ConnectionString");
 
-            dataRequest.Fetch("[dbo].[usp_Test_Select]", JsonConvert.SerializeObject(new { Id = 1 }));
+            string result = dataRequest.Fetch("[dbo].[usp_Test_Select]", JsonConvert.SerializeObject(new { Id = 1 }));
+
+            Assert.IsFalse(string.IsNullOrWhiteSpace(result));
+        }
+
+        [TestMethod]
+        public void ExecuteUpdateTests()
+        {
+            SqlDataRequest dataRequest = new SqlDataRequest("ConnectionString");
+
+            var obj = new
+            {
+                Id = 1,
+                Name = string.Format("Test :{0}", DateTime.Now.ToLongDateString()),
+                Description = string.Format("Description :{0}", DateTime.Now.ToLongDateString()),
+                Details = string.Format("Details :{0}", DateTime.Now.ToLongDateString()),
+                RecordedDate = DateTime.Now,
+                IsActive = true
+            };
+            
+            dataRequest.Update("[dbo].[usp_Test_Insert]", obj);
+
+            var obj2 = new
+            {
+                Id = 2,
+                Name = string.Format("Test :{0}", DateTime.Now.ToLongDateString()),
+                Description = string.Format("Description :{0}", DateTime.Now.ToLongDateString()),
+                Details = string.Format("Details :{0}", DateTime.Now.ToLongDateString()),
+                RecordedDate = DateTime.Now,
+                IsActive = true
+            };
+
+            dataRequest.Update("[dbo].[usp_Test_Insert]", JsonConvert.SerializeObject(obj2));
         }
     }
 }
