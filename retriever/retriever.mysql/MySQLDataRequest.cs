53f1462273646615bd3b3bc29fd392621a7eb37b
KhabileP <KhabileP@EDIMS-KHABILE>
2016-07-15 16:17:36 -0400

    Added datarequest classes for both MySQL and Oracle but with no implemenetation yet.

---
+++ retriever/retriever.mysql/MySQLDataRequest.cs
@@ -0,0 +1,67 @@
+﻿using Retriever.Core;
+using System;
+using System.Collections.Generic;
+
+namespace Retriever.MySQL
+{
+    public class MySQLDataRequest : IDataRequest
+    {
+        public string ConnectionString
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Fetch(string storedProcedureName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Fetch(string storedProcedureName, dynamic paramsObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Fetch(string storedProcedureName, string jsonFetchParams)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Hurl(string storedProcedureName, List<dynamic> objects)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Hurl(string storedProcedureName, dynamic obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Hurl(string storedProcedureName, string jsonData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Hurl(string storedProcedureName, List<dynamic> objects, TransactionMode transMode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Hurl(string storedProcedureName, dynamic obj, TransactionMode transMode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Hurl(string storedProcedureName, string jsonData, TransactionMode transMode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
