4c6ee1eb4d510bb63398e9f792f7ad5d4cd95a92
Pravin Khabile <khabilepravin@hotmail.com>
2017-09-07 12:18:16 -0400

    Updated the interface and added skelton implementations for Oracle and MYSQL

--- retriever/retriever.oracle/OracleDataRequest.cs
+++ retriever/retriever.oracle/OracleDataRequest.cs
@@ -1,6 +1,7 @@
 ﻿using Retriever.Core;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Retriever.Oracle
 {
@@ -54,6 +55,47 @@ namespace Retriever.Oracle
         }
 
         public int Hurl(string storedProcedureName, string jsonData, TransactionMode transMode)
+        {
+            throw new NotImplementedException();
+        }
+              
+
+        public async Task<string> FetchAsync(string storedProcedureName, dynamic paramsObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> FetchAsync(string storedProcedureName, string jsonFetchParams)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> HurlAsync(string storedProcedureName, List<dynamic> objects)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> HurlAsync(string storedProcedureName, dynamic obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> HurlAsync(string storedProcedureName, string jsonData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> HurlAsync(string storedProcedureName, List<dynamic> objects, TransactionMode transMode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> HurlAsync(string storedProcedureName, dynamic obj, TransactionMode transMode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> HurlAsync(string storedProcedureName, string jsonData, TransactionMode transMode)
         {
             throw new NotImplementedException();
         }
